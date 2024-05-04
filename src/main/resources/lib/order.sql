af3ee277698e41dcbfd35ee12aabb3dd7ffbf118
hegoplay <thmnh113@gmail.com>
2024-05-04 16:09:48 +0700

    Hegoplay fixed address entity

---
+++ src/main/resources/lib/order.sql
@@ -0,0 +1,3 @@
+﻿use cdSeller
+insert into orders(orderId,created_date,shipped_date,number,street,city,zip,customer_id,employee_id,status)
+values('od001',GETDATE() - 3,null,'64',N'Trần Hưng Đạo', 'HCMC','713000','KHPTM45163',null,'PENDING')
